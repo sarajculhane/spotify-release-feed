a0bce3c07c37bef23de92787298b9abc9561efc7
Sara Culhane <sjcully@mac.com>
2020-11-24 10:49:20 -0500

    able to retrieve from a single artist on FE

--- src/FetchAll.js
+++ src/FetchAll.js
@@ -4,24 +4,39 @@ import axios from 'axios'
 const FetchAll = (props) => {
     const {ids} = props
     const [tracks, setTracks] = useState({})
+    const [token, setToken] = useState('')
+
+    useEffect(() => {
+        const fetchToken = async () => {
+                try {
+                    const {data} = await axios.get('/access')
+                    setToken(data)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        fetchToken()
+    }, [])
 
     useEffect(() => {
         const fetchTracks = async () => {
                 try {
-                    const res = await axios.get('/access')
-                    const access_token = res.data
-                    const {data} = await axios.get(`https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?offset=0&limit=2&include_groups=appears_on&market=US`, {
-                        header: {
-                            'Authorization': 'Bearer ' + access_token
+                    if(token) { 
+                        console.log(token)
+                        const {data} = await axios.get(`https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums`, {
+                        headers: {
+                        "Authorization": `Bearer ${token}`
                         }
                     })
                     console.log(data)
+
+                }
             } catch(err) {
                 console.log(err)
             }
         }
         fetchTracks()
-    }, [])
+    }, [token])
 
     return (
         <div>from fetchTracks</div>
