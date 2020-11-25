d9a23228ebe57bd94ee490c992705ec4e4422125
Sara Culhane <sjcully@mac.com>
2020-11-25 12:31:35 -0500

    readme file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1> Most recent tracks by Artists you follow</h1>
         <GetData />
       </div>
     );
