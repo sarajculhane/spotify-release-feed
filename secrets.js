fc750f5d20b9de4d7c110accc4dfa8d3a52fe8f5
Sara Culhane <sjcully@mac.com>
2020-12-13 09:06:27 -0500

    Deploying

---
+++ secrets.js
@@ -0,0 +1,8 @@
+var client_id = 'b1dd47b15c6443f3ac8af7dcd8947d93'; // Your client id
+var client_secret = '09f80ef7a0c442729726750eff49358a';
+
+
+module.exports = {
+    client_id,
+    client_secret
+}
